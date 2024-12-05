1cef97afd39b994d4783cb5c34fbbd92e3969c36
Paull <65183959+deltaromeo780@users.noreply.github.com>
2024-12-05 16:25:11 +0100

    Add files via upload
    personal assistant program

---
+++ Application-Personal-Assistant-main/application_personal_assistant/command_assistant.py
@@ -0,0 +1,18 @@
+import Levenshtein
+
+
+def command_assistant(input_text):
+    suggestions = [
+       "add", "all", "names", "address", "phone", "email", "birthday", "edit", "delete", "search",
+        "show", "good bye", "exit", "close", "help"
+    ]
+    closest_match = None
+    min_distance = float('inf')
+
+    for suggestion in suggestions:
+        distance = Levenshtein.distance(input_text, suggestion)
+        if distance < min_distance:
+            min_distance = distance
+            good_command = suggestion
+    print(f" Bad Command: {input_text}, Correct Command is: {good_command} ")
+    return good_command
